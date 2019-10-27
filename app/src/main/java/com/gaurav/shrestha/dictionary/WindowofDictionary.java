e0041c5e2f2707cb966c67ad9b4cad68f31483e8
ShresthaGaurav <gauravshrestha202@gmail.com>
2019-10-27 10:19:36 +0000

    final binding of code v1

--- app/src/main/java/com/gaurav/shrestha/dictionary/WindowofDictionary.java
+++ app/src/main/java/com/gaurav/shrestha/dictionary/WindowofDictionary.java
@@ -11,8 +11,6 @@ import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Switch;
 import android.widget.TextView;
-import android.widget.Toast;
-
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
@@ -27,7 +25,7 @@ public class WindowofDictionary extends AppCompatActivity implements View.OnClic
     private Map<String, String> searchValue;
     private Map<String, String> dayValue;
     private Map<String, String> Search;
-    String countrie[] = {"accidial", "dial someone's number on phone accidentally",
+    String searchWord[] = {"accidial", "dial someone's number on phone accidentally",
             "agender  ", "people do not identify as male or female",
             "airball  ", "completely miss the basket, rim, and backboard with a shot",
             "automagically", "in a way that seems magical, especially by computer",
@@ -65,53 +63,29 @@ public class WindowofDictionary extends AppCompatActivity implements View.OnClic
         dayList = findViewById(R.id.LV2);
         SetDayWord();
         Search = new HashMap<String, String>();
-        for (int i = 0; i < countrie.length; i += 2) {
-            Search.put(countrie[i], countrie[i + 1]);
+        for (int i = 0; i < searchWord.length; i += 2) {
+            Search.put(searchWord[i], searchWord[i + 1]);
             ArrayAdapter arraySearch = new ArrayAdapter(WindowofDictionary.this,
                     android.R.layout.simple_expandable_list_item_1, new ArrayList<String>(Search.keySet()));
             searchText.setAdapter(arraySearch);
+            searchText.setThreshold(1);
         }
 
     }
 
     private void SetDayWord() {
-        final String countries[] = {"accidial", "dial someone's number on phone accidentally",
-                "agender  ", "people do not identify as male or female",
-                "airball  ", "completely miss the basket, rim, and backboard with a shot",
-                "automagically", "in a way that seems magical, especially by computer",
-                "awesomesauce", "extremely good; excellent",
-                "bargainous", "costing less than expected, cheap or relatively cheap",
-                "barista  ", "a person whose job involves preparing and serving different types of coffee",
-                "bedunged  ", "has been soiled with or covered in dung; very old or old-fashioned.",
-                "binge-watch  ", "watch multiple episodes of a television program in rapid succession",
-                "bitcoin", "an online payment system that does not require an intermediary",
-                "cheesebal", "lacking taste, style, or originality",
-                "chillax", "calm down and relax",
-                "consider", "deem to be", "minute", "infinitely or immeasurably small",
-                "Castor oil ", "oil from castor seeds",
-                "Casual", "accidental, chance",
-                "Casualty  ", "accident, mishap, a person injured by accident",
-                "Cat ", " a carnivorous domestic animal, pussy",
-                " Cataclysm ", "deluge",
-                "Catalogue", "a list",
-                " Celibacy   ", "the state of being unmarried",
-                "Celestial  ", "heavenly",
-                " Celebrity  ", "a well-known person",
-                "Celebration ", "observance or performance with due rites",
-                "Celebrated ", "famous",
-                "Celebrate", "perform with proper rites and ceremonies",
-                " Ceiling ", "the inner roof of a house"};
+
         dayValue = new HashMap<String, String>();
         Random r = new Random();
-        int ran = r.nextInt(24 - 0) + 1;
+        int ran = r.nextInt(24 - 5) + 1;
         if (ran % 2 == 0) {
             num = ran;
         } else {
             num = ran - 1;
         }
-        android.widget.Toast.makeText(this, "number is " + num, Toast.LENGTH_SHORT).show();
-        for (int i = num; i < countries.length; i += 2) {
-            dayValue.put(countries[i], countries[i + 1]);
+
+        for (int i = 0; i < num; i += 2) {
+            dayValue.put(searchWord[i], searchWord[i + 1]);
             ArrayAdapter arrayAdapter = new ArrayAdapter(WindowofDictionary.this,
                     android.R.layout.simple_expandable_list_item_1, new ArrayList<String>(dayValue.keySet()));
             dayList.setAdapter(arrayAdapter);
@@ -119,8 +93,8 @@ public class WindowofDictionary extends AppCompatActivity implements View.OnClic
                 @Override
                 public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                     String word = parent.getItemAtPosition(position).toString();
-                    // Toast.makeText(WindowofDictionary.this, "hello"+word, Toast.LENGTH_SHORT).show();
-                    searchList.setText("Your search item is  : " + word + "\n" + "meaning  : " + dayValue.get(word));
+                    searchText.setText(word);
+                    searchList.setText("\n Your search item is  : " + word + "\n\n" + " Meaning  : " + dayValue.get(word));
                 }
             });
 
@@ -133,14 +107,14 @@ public class WindowofDictionary extends AppCompatActivity implements View.OnClic
         switch (v.getId()) {
             case R.id.Btnsearch:
                 String searchtxt = searchText.getText().toString();
-                for (int i = 0; i < countrie.length; i += 2) {
-                    if (searchtxt.equalsIgnoreCase(countrie[i])) {
+                for (int i = 0; i < searchWord.length; i += 2) {
+                    if (searchtxt.equalsIgnoreCase(searchWord[i])) {
                         searchValue = new HashMap<String, String>();
-                        searchValue.put(countrie[i], countrie[i + 1]);
-                        searchList.setText("Your search item is  : " + countrie[i] + "\n" + "meaning  : " + searchValue.get(countrie[i]));
+                        searchValue.put(searchWord[i], searchWord[i + 1]);
+                        searchList.setText("\n Your search item is  : " + searchWord[i] + "\n\n" + " Meaning  : " + searchValue.get(searchWord[i]));
                         return;
                     } else {
-                        searchList.setText("Your search item not found  :\n" + searchtxt);
+                        searchList.setText("\n Your search item not found  :\n" + searchtxt);
 
                     }
 
